6a80578cfc162bf5378a54f66ffb601baae4d9d8
Karim Farahat <karim.f.farahat@pwc.com>
2023-12-21 17:51:15 +0200

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { NavigationProvider } from "./context/navigation";
+// get root el
+const el = document.getElementById("root");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// give control to ReactDOM
+const root = ReactDOM.createRoot(el);
+
+// render app in root
 root.render(
-  <React.StrictMode>
+  <NavigationProvider>
     <App />
-  </React.StrictMode>
+  </NavigationProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
